7f2ac2ec56b7e974f2672021319f0a54eb4be87c
Bryan Mytko <bryanmytko@gmail.com>
2016-06-25 00:53:06 -0400

    Add vcr/nock for mocha testing

--- test/endpoints.js
+++ test/endpoints.js
@@ -2,7 +2,11 @@ var chai = require('chai'),
   expect = chai.expect,
   util   = require('util'),
   sinon  = require('sinon'),
-  sinonChai = require('sinon-chai');
+  sinonChai = require('sinon-chai'),
+  vcr = require('nock-vcr-recorder-mocha');
+
+// @TODO cache API requesets:
+// vcr.it('does an API call', function(done){ ...
 
 var envVars = require('./env-vars'),
   access_token = envVars['BLIZZARD_ACCESS_TOKEN'];
